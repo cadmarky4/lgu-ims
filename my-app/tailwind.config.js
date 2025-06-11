8beed025379d6ecd2203e39887dd582e119c3672
johnmarkgatche <johngatchemark@gmail.com>
2025-06-12 02:01:40 +0800

    partially implemented front-end UI for the reports page

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,4 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
-}; 
+};
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         smblue: {
+//           50: "#F3FFF0",
+//           100: "#8FDBC5",
+//           200: "#64ADC4",
+//           300: "#4887B7",
+//           400: "#367096",
+//         },
+//       },
+//     },
+//   },
+// };
