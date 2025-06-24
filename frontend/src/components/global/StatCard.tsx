7f9cb7d7b0fc3d7f81ac751819dd3abbd1621bde
johnmarkgatche <johngatchemark@gmail.com>
2025-06-24 13:19:59 +0800

    fixed stat card height to take up the full height of the container

--- frontend/src/components/global/StatCard.tsx
+++ frontend/src/components/global/StatCard.tsx
@@ -13,7 +13,7 @@ const StatCard: React.FC<StatCardProps> = ({
   icon: Icon
 }) => {
   return (
-    <article className="flex p-4 bg-stats-card rounded-xl justify-between items-center">
+    <article className="h-full flex p-4 bg-stats-card rounded-xl justify-between items-center">
       <section className="flex flex-col gap-1.5">
         <h5 className="text-sm text-smblue-400">{title}</h5>
         <h2 className="text-2xl font-bold">
